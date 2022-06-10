69df282b84bdcef242a9e2666b6a57f872cd04bb
Ethan Schurkman <ethan.a.schurkman@gmail.com>
2022-06-10 00:36:20 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+let activeRating = null;
+
+document.querySelector('.thank-you').style.display = 'none';
+
+const ratings = document.querySelectorAll('.rating');
+ratings.forEach((rating) => {
+    rating.addEventListener('click', () => {
+        if (activeRating === rating) {
+            return;
+        }
+
+        if (activeRating != null) {
+            activeRating.classList.toggle('active');
+        }
+        rating.classList.toggle('active');
+        activeRating = rating;
+    })
+});
+
+const submitBtn = document.querySelector('.submit-btn');
+
+submitBtn.addEventListener('click', () => {
+    if (activeRating == null) {
+        return;
+    }
+    document.querySelector('.score').innerHTML = activeRating.innerHTML;
+    document.querySelector('.survey').style.display = 'none';
+    document.querySelector('.thank-you').style.display = 'block';
+});
